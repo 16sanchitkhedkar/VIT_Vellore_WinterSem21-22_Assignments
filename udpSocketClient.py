9ca2059462a05e8ea0fc07e6fb9f1c903c60c637
16sanchitkhedkar <73767747+16sanchitkhedkar@users.noreply.github.com>
2022-04-27 10:50:54 +0530

    Modified UDP Socket Boilerplate code

--- udpSocketClient.py
+++ udpSocketClient.py
@@ -1,20 +1,8 @@
 import socket
-serverAddressPort = ("127.0.0.1", 3000)
-bufferSize = 1024
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-if UDPClientSocket:
-    while True:
-        fname = input("Enter first name: ")
-        lname = input("Enter last name: ")
-        year = int(input("Enter birth year: "))
-        month = int(input("Enter birth month: "))
-        day = int(input("Enter birth day: "))
-        msgFromClient = fname + " " + lname + " " + str(year) + " " + str(month) + " " + str(day)
-        bytesToSend = str.encode(msgFromClient)
-        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-        msg = "Message from Server {}".format(msgFromServer[0].decode('utf-8'))
-        print(msg)
-        toCont = input("Do you want to continue or enter exit to stop: ")
-        if toCont.lower() == "exit":
-            break
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = "Hello, World!"
+sock = socket.socket(socket.AF_INET, # Internet
+socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+print (sock.recv(1024).decode())
--- udpSocketServer.py
+++ udpSocketServer.py
@@ -1,26 +1,10 @@
 import socket
-from datetime import date, datetime
-def calculate_age(born):
-    today = date.today()
-    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
-localIP     = "127.0.0.1"
-localPort   = 3000 
-bufferSize  = 1024
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket.bind((localIP, localPort))
-print("UDP Server Started at port 3000 at address 127.0.0.1")
-while(True):
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0].decode('utf-8')
-    address = bytesAddressPair[1]
-    clientInfo = message.split(' ')
-    clientName = clientInfo[0] + " " + clientInfo[1]
-    birthday = datetime( int(clientInfo[2]), int(clientInfo[3]), int(clientInfo[4]))
-    age = calculate_age(birthday)
-    clientMsg = "Client informationt: Name = {}, Birthdate: {}AGE : {} years".format(clientName, birthday, age)
-    clientIP  = "Client IP Address: {}".format(address)
-    print(clientMsg)
-    print(clientIP)
-    serverResponse = clientMsg      
-    bytesToSend = str.encode(serverResponse)
-    UDPServerSocket.sendto(bytesToSend, address)
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET, # Internet
+socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+while True:
+data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+print ("received message:", data.decode())
+sock.sendto("Hello".encode(),addr)
